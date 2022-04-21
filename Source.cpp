06880762a8abbae11a4a3a07f66ebb0233462059
Stepan <stdnsv@gmail.com>
2022-04-21 13:22:26 +0400

    Heap sort

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -54,50 +55,129 @@ stats bubble_sort(vector<int>& data)
 	return res;
 }
 
-stats quick_sort(vector<int>& data, int left, int right)
+void rquick_sort(vector<int>& data, int left, int right, stats& res)
 {
-	int l = left; //лева€ граница
-	int r = right; // права€ граница
+	int l = left;
+	int r = right;
 	int cur = data[left];
-	while (left < right) // пока границы не сомкнутс€
+	while (left < right)
 	{
 		while ((data[right] >= cur) && (left < right))
-			right--; // сдвигаем правую границу пока элемент [right] больше [pivot]
-		if (left != right) // если границы не сомкнулись
 		{
-			data[left] = data[right]; // перемещаем элемент [right] на место разрешающего
-			left++; // сдвигаем левую границу вправо
+			res.comparison_count++;
+			right--;
+		}
+		if (left < right)
+		{
+			data[left] = data[right];
+			res.copy_count++;
+			left++;
 		}
 		while ((data[left] <= cur) && (left < right))
-			left++; // сдвигаем левую границу пока элемент [left] меньше [pivot]
-		if (left != right) // если границы не сомкнулись
 		{
-			data[right] = data[left]; // перемещаем элемент [left] на место [right]
-			right--; // сдвигаем правую границу влево
+			res.comparison_count++;
+			left++;
+		}
+		if (left < right)
+		{
+			data[right] = data[left];
+			res.copy_count++;
+			right--;
 		}
 	}
-	data[left] = cur; // ставим разрешающий элемент на место
+	data[left] = cur;
 	cur = left;
 	left = l;
 	right = r;
-	if (left < cur) // –екурсивно вызываем сортировку дл€ левой и правой части массива
-		quick_sort(data, left, cur - 1);
+	if (left < cur)
+		rquick_sort(data, left, cur - 1, res);
 	if (right > cur)
-		quick_sort(data, cur + 1, right);
-	stats s;
-	return s;
+		rquick_sort(data, cur + 1, right, res);
 }
 
+stats quick_sort(vector<int>& data)
+{
+	int left = 0;
+	int right = data.size() - 1;
+	stats res;
+	rquick_sort(data, left, right, res);
+	return res;
+}
+
+void heap(vector<int>& data, int n, int i, stats& res)
+{
+	int largest = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+
+	if (l < n && data[l] > data[largest])
+	{
+		res.comparison_count++;
+		largest = l;
+	}
+
+	if (r < n && data[r] > data[largest])
+	{
+		res.comparison_count++;
+		largest = r;
+	}
+
+	if (largest != i) {
+		int tmp = data[largest];
+		data[largest] = data[i];
+		data[i] = tmp;
+		res.copy_count++;
+		heap(data, n, largest, res);
+	}
+}
+
+stats heap_sort(vector<int>& data) //сложность примерно верна€
+{
+	stats res;
+	for (int i = data.size() / 2 - 1; i >= 0; i--)
+		heap(data, data.size(), i, res);
+
+	for (int i = data.size() - 1; i >= 0; i--) {
+		//res.copy_count++;
+		int tmp = data[0];
+		data[0] = data[i];
+		data[i] = tmp;
+		heap(data, i, 0, res);
+	}
+	return res;
+}
+
+int random()
+{
+	return round(rand() % 21);
+}
 
 int main()
 {
-	vector<int> arr = {5,6,3,8};
-	//stats s = bubble_sort(arr);
-	stats s = quick_sort(arr, 0, arr.size()-1);
-	for (auto j = arr.begin(); j != arr.end(); j++)
+	const int size = 1000;
+	const int count = 100;
+	stats results[count];
+	vector<vector<int>> ch_arr[count];
+	for (int i = 0; i < count; i++)
 	{
-		cout << *j << ' ';
+		vector<int> v(size);
+		generate(v.begin(), v.end(), random);
+		ch_arr->push_back(v);
 	}
-	cout << endl << s.comparison_count << endl << s.copy_count;
-	return 0;
+
+	for (int i = 0; i < count; i++)
+	{
+		results[i] = quick_sort(ch_arr->at(i));
+	}
+
+	int sum_comparison = 0;
+	int sum_copy = 0;
+	for (int i = 0; i < count; i++)
+	{
+		sum_comparison += results[i].comparison_count;
+		sum_copy += results[i].copy_count;
+	}
+	sum_comparison = sum_comparison / count;
+	sum_copy = sum_copy / count;
+	cout << sum_comparison << ' ' << sum_copy;
 }
